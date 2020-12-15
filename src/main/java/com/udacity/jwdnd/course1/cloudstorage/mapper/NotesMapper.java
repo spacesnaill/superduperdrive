8a88ab93e5992d66598d3d481523da32a94b94fc
Patrick Geiger <patrickjgeiger@gmail.com>
2020-12-15 10:23:46 -0500

    ✨ added both select notes by userid and insert new note to mapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NotesMapper.java
@@ -1,2 +1,19 @@
-package com.udacity.jwdnd.course1.cloudstorage.mapper;public interface NotesMapper {
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface NotesMapper {
+    @Select("SELECT * FROM NOTES WHERE USERID=#{userid}")
+    List<Notes> getNotesByUserId(Integer userid);
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{notetitle}, #{notedescription}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteid")
+    int insertNote(Notes note);
 }
