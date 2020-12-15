2500e311bbcffe1413766a19ffcd8a454759b88a
Patrick Geiger <patrickjgeiger@gmail.com>
2020-12-15 10:24:29 -0500

    ✨ added service for handling the creation and retrieval of notes from mapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NotesService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NotesService.java
@@ -1,2 +1,26 @@
-package com.udacity.jwdnd.course1.cloudstorage.services;public class NotesService {
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NotesMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotesService {
+
+    private final NotesMapper notesMapper;
+
+    public NotesService(NotesMapper notesMapper) {
+        this.notesMapper = notesMapper;
+    }
+
+    public List<Notes> getNotesByUserId(Integer userId) {
+        return notesMapper.getNotesByUserId(userId);
+    }
+
+    public int createNote(Notes note) {
+        return notesMapper.insertNote(note);
+    }
+
 }
