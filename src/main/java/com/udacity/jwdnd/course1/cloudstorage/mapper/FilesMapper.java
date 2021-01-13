35d472829cc940654b00c2b607baae90576b7f81
Patrick Geiger <patrickjgeiger@gmail.com>
2021-01-13 10:45:07 -0500

    🗃  added get, insert, and delete mapper functions for Files table

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FilesMapper.java
@@ -0,0 +1,20 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Files;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface FilesMapper {
+
+    @Select("SELECT * FROM FILES WHERE USERID=#{userid}")
+    List<Files> getFilesByUserId(Integer userid);
+
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata) VALUES(#{filename}, #{contenttype}, #{filesize}, #{userid}, #{filedata})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileid")
+    int insertFile(Files file);
+
+    @Delete("DELETE FROM FILES WHERE FILEID=#{fileid}")
+    int deleteFile(Integer fileid);
+}
