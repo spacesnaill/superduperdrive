4bdc29fd31c06cb01c85189940989400cdd7adcf
Patrick Geiger <patrickjgeiger@gmail.com>
2021-01-21 10:21:11 -0500

    ✅ added tests for creating a new note
    also added helper function for switching to the notes tab

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -20,6 +20,11 @@ class CloudStorageApplicationTests {
 
 	private WebDriver driver;
 
+	private final static String firstName = "Jane";
+	private final static String lastName = "Doe";
+	private final static String userName = "Janedoe";
+	private final static String password = "password";
+
 	@BeforeAll
 	static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
@@ -55,6 +60,8 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("Login", driver.getTitle());
 	}
 
+	// This could technically use the helper functions, but they would circumvent some button interactions that I want
+	// to test.
 	@Test
 	public void userSignUpAndLogIn() {
 		driver.get("http://localhost:" + this.port + "/login");
@@ -69,11 +76,6 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("Sign Up", driver.getTitle());
 
 		// Signing up
-		String firstName = "Jane";
-		String lastName = "Doe";
-		String userName = "Janedoe";
-		String password = "password";
-
 		WebElement firstNameInput = driver.findElement(By.id("inputFirstName"));
 		WebElement lastNameInput = driver.findElement(By.id("inputLastName"));
 		WebElement userNameInput = driver.findElement(By.id("inputUsername"));
@@ -118,8 +120,87 @@ class CloudStorageApplicationTests {
 
 
 	// Adding, editing, and deleting notes tests
+	@Test
+	public void notesTests() {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		signUp();
+		signIn();
+		driver.get("http://localhost:" + this.port + "/home");
 
+		switchToNotesTab();
+
+		WebElement addNoteButton = driver.findElement(By.id("add-note"));
+		addNoteButton.click();
+
+		String noteTitle = "Note";
+		String noteDescription = "Hello World";
+
+		WebElement noteTitleInput = driver.findElement(By.id("note-title"));
+		WebElement noteDescriptionInput = driver.findElement(By.id("note-description"));
+
+		wait.until(ExpectedConditions.visibilityOf(noteTitleInput));
+
+		noteTitleInput.sendKeys(noteTitle);
+		noteDescriptionInput.sendKeys(noteDescription);
+		noteDescriptionInput.submit();
+
+		Assertions.assertEquals("Result", driver.getTitle());
+		Assertions.assertEquals("Success", driver.findElement(By.tagName("h1")).getText());
+
+		// Check to make sure it saved and now renders the new note
+		driver.get("http://localhost:" + this.port + "/home");
+		switchToNotesTab();
+		Assertions.assertEquals(noteTitle, driver.findElement(By.xpath("//tbody/tr/th")).getText());
+		Assertions.assertEquals(noteDescription, driver.findElement(By.xpath("//td[2]")).getText());
+	}
 
 	// Adding, editing, and deleting credentials tests
 
+
+	// Helper Functions
+	private void signUp() {
+		driver.get("http://localhost:" + this.port + "/signup");
+
+		WebElement firstNameInput = driver.findElement(By.id("inputFirstName"));
+		WebElement lastNameInput = driver.findElement(By.id("inputLastName"));
+		WebElement userNameInput = driver.findElement(By.id("inputUsername"));
+		WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+
+		firstNameInput.sendKeys(firstName);
+		lastNameInput.sendKeys(lastName);
+		userNameInput.sendKeys(userName);
+		passwordInput.sendKeys(password);
+		passwordInput.submit();
+	}
+
+	private void signIn() {
+		driver.get("http://localhost:" + this.port + "/login");
+
+		// Logging in
+		WebElement usernameLogin = driver.findElement(By.id("inputUsername"));
+		WebElement passwordLogin = driver.findElement(By.id("inputPassword"));
+
+		usernameLogin.sendKeys(userName);
+		passwordLogin.sendKeys(password);
+		passwordLogin.submit();
+	}
+
+	private void switchToNotesTab() {
+		WebDriverWait wait = new WebDriverWait(driver, 3);
+
+		// Click on the Notes tab button
+		WebElement notesNavButton = driver.findElement(By.id("nav-notes-tab"));
+		wait.until(ExpectedConditions.visibilityOf(notesNavButton));
+		notesNavButton.click();
+
+		// Verify we've switched tabs and the elements within are visible
+		WebElement addNoteButton = driver.findElement(By.id("add-note"));
+		wait.until(ExpectedConditions.visibilityOf(addNoteButton));
+	}
+
+	private void switchToCredentialsTab() {
+		WebDriverWait wait = new WebDriverWait(driver, 3);
+
+
+	}
 }
